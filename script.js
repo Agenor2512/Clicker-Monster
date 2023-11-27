0bacde9ae0c9cd52febfc017eef454d2042294c8
Agenor2512 <noemie.vasseur2512@gmail.com>
2023-11-27 21:49:13 +0100

    Adding footer with selected button highlighted

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => { 
+
+    buttons.forEach(button => {
+        button.classList.remove("selected");
+    });
+    
+    button.classList.add("selected");
+    });
+});
+
