2120e01a53ac72ea07590b24209224505cd4f355
Margot Masset <margot.voydis@gmail.com>
2023-11-29 17:42:22 +0100

    username

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => { 
+    button.addEventListener('click', () => {
 
-    buttons.forEach(button => {
-        button.classList.remove("selected");
-    });
-    
-    button.classList.add("selected");
+        buttons.forEach(button => {
+            button.classList.remove("selected");
+        });
+
+        button.classList.add("selected");
     });
 });
+
+// récupère la valeur
+// utilisation du local storage pour stocker le nom du joueur //
+
+const input = document.querySelector("input");
+const choose = document.querySelector("#choose");
+choose.addEventListener('click', () => {
+    const username = input.value
+    console.log(username);
+});
+
+//==========================================================/
