5609694aa08a0aa933da171e41641302aff5e663
micka <mickaelfalsaperla@hotmail.com>
2023-12-01 10:46:56 +0100

    added auto-clic function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll("button");
 
 let clicksCounter = 0;
-let autoClicksCounter = 0;
+let autoClicksCounter = 10;
 let totalClicksCounter = 0;
 let currentXP = 0;
 
@@ -36,6 +36,18 @@ eggSprite.addEventListener('click', function() {
 });
 
 /********************************/
+/********* Auto-clicker *******/
+/********************************/
+
+function startAutoClicker() {
+    setInterval(function() {
+        currentXP += autoClicksCounter;
+    }, 1000);
+}
+
+startAutoClicker();
+
+/********************************/
 /********* Progress Bar *********/
 /********************************/
 
@@ -54,7 +66,7 @@ eggSprite.addEventListener('click', function() {
 }
 
 /* Décrit le passage au niveau suivant 
-   Remaining XP permet "d'arondir" l'XP pour qu'il corresponde à l'XP dont 
+   Remaining XP permet "d'arrondir" l'XP pour qu'il corresponde à l'XP dont 
    il y a besoin pour passer le niveau courant 
    -> temporary est une variable temporaire qui permet de conserver le niveau courant
    et le niveau précédent pour les intervertir et simuler le passage de niveaux */
