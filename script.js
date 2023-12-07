04650af7dd4586af65e74132cddc792632b4ebe7
Agenor2512 <noemie.vasseur2512@gmail.com>
2023-12-07 14:36:33 +0100

    Modify paths for github pages

--- script.js
+++ script.js
@@ -81,32 +81,32 @@ const changerImageSelonNiveau = () => {
   // Vérification du niveau pour changer l'image
   switch (true) {
     case userLvl >= 1 && userLvl <= 10:
-      spriteChange.src = '/assets/golden-egg.png';
+      spriteChange.src = 'assets/golden-egg.png';
       break;
     case userLvl >= 11 && userLvl <= 20:
-      spriteChange.src = '/assets/cracked-egg.png';
+      spriteChange.src = 'assets/cracked-egg.png';
       break;
     case userLvl >= 21 && userLvl <= 24:
-      spriteChange.src = '/assets/broken-egg.png';
+      spriteChange.src = 'assets/broken-egg.png';
       spriteChange.style.height = '17rem';
       spriteChange.style.width = '15rem';
       spriteChange.style.marginTop = '4rem';
       break;
     case userLvl >= 25 && userLvl <= 27:
-      spriteChange.src = '/assets/turtle-baby.png';
+      spriteChange.src = 'assets/turtle-baby.png';
       spriteChange.style.height = '18rem';
       spriteChange.style.width = '15rem';
       spriteChange.style.marginTop = '4rem';
       break;
     case userLvl >= 26 && userLvl <= 30:
-      spriteChange.src = '/assets/turtle-adult.png';
+      spriteChange.src = 'assets/turtle-adult.png';
       spriteChange.style.height = '19rem';
       spriteChange.style.width = '17rem';
       spriteChange.style.marginTop = '4rem';
       break;
     // Ajoutez d'autres cas pour chaque niveau si nécessaire
     default:
-      spriteChange.src = '/assets/turtle-adult.png'; // Image par défaut si le niveau n'est pas géré
+      spriteChange.src = 'assets/turtle-adult.png'; // Image par défaut si le niveau n'est pas géré
       spriteChange.style.height = '19rem';
       spriteChange.style.width = '17rem';
       spriteChange.style.marginTop = '4rem';
